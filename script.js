bdfb755bdc5db70f18c338c022bf6ccc3f053d07
Shakib-Shajid <shakibalshajid1682@gmail.com>
2023-09-01 21:03:09 +0600

    card show from api

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// category api part
 const handleCategory = async () => {
   const response = await fetch(
     "https://openapi.programming-hero.com/api/videos/categories"
@@ -8,9 +9,58 @@ const handleCategory = async () => {
   data.data.forEach((category) => {
     const div = document.createElement("div");
     div.innerHTML = `
-      <a class="tab tab-active">${category.category}</a> `;
-    console.log(category);
+    <a onclick="handleCard(${category.category_id})" class="tab">${category.category}</a>
+    `;
     tabContainer.appendChild(div);
   });
 };
+
+// card api part
+const handleCard = async (categoryId) => {
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${categoryId}`
+  );
+  const data = await response.json();
+  // console.log(data);
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = "";
+
+  data.data.forEach((category) => {
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+    <div class="card card-compact w-72 bg-base-100 shadow-xl">
+        <figure><img src=${
+          category?.thumbnail
+        } alt="Shoes" class="rounded-lg w-80 h-52" /></figure>
+        <div class="card-body">
+      <div class="flex gap-3">
+        <div>
+          <figure><img class="rounded-full w-10 h-10" src=${
+            category?.authors[0]?.profile_picture
+          } /></figure>
+        </div>
+        <div>
+          <h2 class="card-title">${category?.title}</h2>
+          <p class="py-2">${
+            category?.authors[0]?.profile_name
+          }                        <span  class="pl-1 text-blue-600">
+          ${
+            category?.authors[0]?.verified
+              ? '<i class="fa-solid fa-circle-check"></i>'
+              : ""
+          }
+        
+         </span></p> 
+          <p>${category?.others?.views}</p>
+        </div>
+      </div>
+        </div>
+    </div>
+    `;
+    cardContainer.appendChild(div);
+    // console.log(C.appendChild(img));
+  });
+};
 handleCategory();
+handleCard("1000");
